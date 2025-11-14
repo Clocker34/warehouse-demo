c2635707e98209ed2b3977dd35481f5415c657f5
Clocker34 <paramonovarseniy@gmail.com>
2025-11-14 20:38:15 +0300

    Add Stock entity with ManyToOne relationships

---
+++ src/main/java/com/example/warehouse/entity/Stock.java
@@ -0,0 +1,26 @@
+package com.example.warehouse.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+    
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+    
+    private int quantity;
+}
